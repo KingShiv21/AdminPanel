9923a02526e0198aacf881929076a8e5a9bef4dc
KingShiv21 <shivrat503@gmail.com>
2024-02-10 21:35:58 +0530

    server ready

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const express = require("express")
 
 const app = express()
+const rateLimit = require('express-rate-limit')
+const path = require('path')
+const ejs = require('ejs')
 
 require('dotenv').config()
+
+
+// middlewares
+app.use(rateLimit({
+    windowMs : 15 * 60 * 1000,
+    max : 1000
+}))
+app.use(express.json({
+    limit:'5mb'
+}))
+app.use(express.json())
+app.use('/', express.static(path.join(__dirname , 'public')))   // for serving static files to the /filename api requests
+app.set('view engine' , 'ejs')
+app.set('views' ,  path.join(__dirname , 'public' , 'views'))
+
+
+// routes
+
+
+
+
+
+
 
 app.get("/", (req,res)=>{
     res.send("Server is Fine")
 })
-
-
+app.get("*", async(req,res)=>{
+    res.send({
+        status : "6320" ,
+        Backend_Error : "There in no route like this , hit the correct route"
+    })
+})
 app.listen(process.env.PORT , ()=>{
     console.log(`Server Started on port ${process.env.PORT}`)
 })
